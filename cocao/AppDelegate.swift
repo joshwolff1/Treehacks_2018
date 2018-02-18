1140f931e5595f539809e26b844b1ead77f01176
Josh Wolff <jw1@DN0a22b503.SUNet>
2018-02-18 00:29:09 -0800

    aws added in app delegate

--- cocao/AppDelegate.swift
+++ cocao/AppDelegate.swift
@@ -8,22 +8,97 @@
 
 import UIKit
 import HoundifySDK
+import AWSCore
 
 // test test 2018
+
+// This actually needs to be here.
+let userPoolID = "SampleUserPool"
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var cognitoConfig:CognitoConfig?
+    
+    // set up the initialized flag
+    // FROM AWS SDK INSTALLATION INSTRUCTIONS
+    var isInitialized: Bool = false
+    // var pinpoint: AWSPinpoint?
+    
+    // FROM AWS SDK INSTALLATION INSTRUCTIONS & SIGN IN INSTRUCTIONS
+    func application(_ application: UIApplication, open url: URL,
+                     sourceApplication: String?, annotation: Any) -> Bool {
+        
+        AWSDDLog.sharedInstance.logLevel = .verbose
+        AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
+        
+        // set up Cognito config
+        self.cognitoConfig = CognitoConfig()
+        // set up Cognito
+        setupCognitoUserPool()
+        
+        return true
+        
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Hound.setClientID("ewb8Zfanqv_n2FmfW5bObA==")
         Hound.setClientKey("gsqdIB8K6qMLx3dNNW9rKTnxPZc9YE4HIVGBGIcWVxjaVs78V5IJi3aJDyk3sOXZ90PXmEjDvJbccRVMNgiTnQ==")
-        let credentialProvider = AWSCognitoCredentialsProvider(regionType: AwsRegion, identityPoolId: CognitoIdentityPoolId)
         
         return true
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        print("didFinishLaunching:")
+        
+        // set up logging for AWS and Cognito
+        AWSDDLog.sharedInstance.logLevel = .verbose
+        AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
+        
+        // set up Cognito config
+        self.cognitoConfig = CognitoConfig()
+        
+        // set up Cognito
+        setupCognitoUserPool()
+        
+        return true
+        
+    }
+    
+    func setupCognitoUserPool() {
+        
+        // we pull the needed values from the CognitoConfig object
+        // this just pulls the values in from the plist
+        let clientId:String = self.cognitoConfig!.getClientId()
+        let poolId:String = self.cognitoConfig!.getPoolId()
+        let clientSecret:String = self.cognitoConfig!.getClientSecret()
+        let region:AWSRegionType = self.cognitoConfig!.getRegion()
+        let identityPoolId:String = self.cognitoConfig!.getIdentityPoolId()
+        
+        // FOR IAM
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType: AWSRegionType.USEast1, identityPoolId: identityPoolId, identityProviderManager: "" as? AWSIdentityProviderManager)
+        // AWSServiceManager.default().defaultServiceConfiguration = serviceConfiguration
+        // AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        // we need to let Cognito know which region we plan to connect to
+        let serviceConfiguration:AWSServiceConfiguration = AWSServiceConfiguration(region: region, credentialsProvider: credentialsProvider)
+        
+        // FOR IAM
+        AWSServiceManager.default().defaultServiceConfiguration = serviceConfiguration
+        
+//        // we need to pass it the clientId and clientSecret from the app and the poolId for the user pool
+//        let cognitoConfiguration:AWSCognitoIdentityUserPoolConfiguration = AWSCognitoIdentityUserPoolConfiguration(clientId: clientId, clientSecret: clientSecret, poolId: poolId, shouldProvideCognitoValidationData: true)
+//        AWSCognitoIdentityUserPool.register(with: serviceConfiguration, userPoolConfiguration: cognitoConfiguration, forKey: userPoolID)
+//        let pool:AWSCognitoIdentityUserPool = AppDelegate.defaultUserPool()
+//        // we need to set the AppDelegate as the user pool's delegate, which will get called when events occur
+//        pool.delegate = self as AWSCognitoIdentityInteractiveAuthenticationDelegate
+        
+        let cognitoId = credentialsProvider.identityId
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
